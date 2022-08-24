fc9f8c355dde95dfbb9a2383c8506f2f424c4343
Jonathan Noyola <wsrchoko@gmail.com>
2022-08-24 08:53:53 -0500

    [FIX] Invoice - date props

--- src/@types/invoice.ts
+++ src/@types/invoice.ts
@@ -8,8 +8,8 @@ export interface Invoice {
   logo: string;
   dateHeaders: string[];
   no: string;
-  date: Date;
-  dueDate: Date;
+  date?: Date;
+  dueDate?: Date;
   itemsHeaders: string[];
   items: Item[];
   amountHeaders: string[];
